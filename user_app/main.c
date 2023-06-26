705b7dc72d27c59785286977ff2e84fd8a5f037c
DESKTOP-3Q6P33L\Luka <luka.vidic01@gmail.com>
2023-06-26 12:24:06 +0200

    User app working with servo

--- user_app/main.c
+++ user_app/main.c
@@ -11,20 +11,32 @@
 const char* RED = "RED";
 const char* YELLOW = "YELLOW";
 const char* GREEN = "GREEN";
+const char* MOV_UP = "b";
+const char* MOV_DOWN = "e";
 const char* LED_DRIVER = "/dev/led_driver";
+const char* PWM_DRIVER = "/dev/pwm_driver";
 
 const int RED_SLEEP = 5; 
 const int YELLOW_SLEEP = 2;
 const int GREEN_SLEEP = 4;  
 
 int send_to_drivers(const char* msg){
-    int led_fd = open(LED_DRIVER, O_RDWR);
+    int led_fd = open(LED_DRIVER, O_WRONLY);
+    int pwm_fd = open(PWM_DRIVER, O_WRONLY)
     if(led_fd < 0){
-        printf("Error, %s not opened\n", LED_DRIVER);
+        printf("ERROR: %s not opened\n", LED_DRIVER);
+        return -1;
+    }
+    if(pwm_fd < 0){
+        printf("ERROR: %s not opened\n", PWM_DRIVER);
         return -1;
     }
     write(led_fd, msg, BUF_LEN);
     close(led_fd);
+    if(strcmp(RED,msg))
+        write(pwm_fd, MOV_DOWN, strlen(MOV_DOWN));
+    else if(strcmp(GREEN,msg))
+        write(pwm_fd, MOV_UP, strlen(MOV_UP));
     return 0;
 }
 
@@ -46,6 +58,11 @@ int main()
             return -1;
         }
         sleep(GREEN_SLEEP);
+        if(send_to_drivers(YELLOW) == -1){
+            perror("FATAL ERROR: Failed writing to drivers");
+            return -1;
+        }
+        sleep(YELLOW_SLEEP);
     }
     return 0;
 }
